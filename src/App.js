713c9ae8a3e497fc5499d57489297e8918867bae
schweizercode <schweizercode@gmail.com>
2021-02-16 19:23:20 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
   state = {
     loading: true,
     teams: []
@@ -12,7 +13,6 @@ export default class App extends React.Component {
     fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=Germany')
       .then(response => response.json())
       .then(data => console.log(data));
-    // console.log(data.teams[0].strountry)
   }
 
 
@@ -37,3 +37,50 @@ export default class App extends React.Component {
 }
 
 // export default App;
+
+
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       isLoading: true,
+//       dataSource: []
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=Germany')
+//       .then(response => response.json())
+//       .then((responseJson) => {
+//         this.setState({
+//           isLoading: false,
+//           dataSource: responseJson
+//         })
+//       })
+
+//   }
+
+//   renderItem = ({ item, index }) => {
+//     return (
+//       <View style={styles.item}>
+//         <Text>Hello</Text>
+//       </View>
+//     )
+//   }
+
+//   render() {
+//     let { container } = styles
+//     let { dataSource, isLoading } = this.state
+//     return (
+//       <view style={container}>
+//         <FlatList
+//           data={dataSource}
+//           renderItem={this.renderItem}
+//           keyExtractor={(item, index) => index.toString()}
+//         />
+//       </view>
+//     )
+//   }
+// }
+
