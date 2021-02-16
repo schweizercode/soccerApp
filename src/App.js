922ec2d39f2125003aa3f5ad124154362d6c72bb
schweizercode <schweizercode@gmail.com>
2021-02-16 16:03:18 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import React from 'react';
 import './App.css';
-import axios from 'axios';
 
-function App() {
-  return (
+export default class App extends React.Component {
+  state = {
+    loading: true,
+    teams: []
+    // strAlternate: { ''}
+  };
 
-    <p>Learn React</p>
+  async componentDidMount() {
+    fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=Germany')
+      .then(response => response.json())
+      .then(data => console.log(data));
+    // console.log(data.teams[0].strountry)
+  }
 
 
-  );
+  render() {
+
+    if (this.state.loading) {
+      return <div>Loading..</div>;
+    }
+
+    if (!this.state.teams.length) {
+      return <div>didn't get a team</div>;
+    }
+
+    return (
+      <div>
+        <div>{this.state.teams.name.title}</div>
+        <div>{this.state.teams.name.title}</div>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+// export default App;
