7fad381181a22e890c07090ddc0b149b99829d86
schweizercode <schweizercode@gmail.com>
2021-02-18 16:59:28 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import './navbar.js';
+import * as ReactBootStrap from 'react-bootstrap';
 
 export default class App extends Component {
   state = {
@@ -31,15 +32,42 @@ export default class App extends Component {
     // }
 
     return (
-      <list class="list">
-        <h1> Teams</h1>
-        {this.state.teams.map(team => (
-          <ol><img class="Icon" src={team.strTeamBadge} alt="Icon"></img>{team.strTeam} </ol>
+      <div className="App">
+        <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <ReactBootStrap.Navbar.Brand href="#home">Soccer Teams</ReactBootStrap.Navbar.Brand>
+          <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+            <ReactBootStrap.Nav className="mr-auto">
+              <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Divider />
+                <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+              </ReactBootStrap.NavDropdown>
+            </ReactBootStrap.Nav>
+            <ReactBootStrap.Nav>
+              <ReactBootStrap.Nav.Link href="#contact">Contact</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link eventKey={2} href="#chat">
+                Chat
+      </ReactBootStrap.Nav.Link>
+            </ReactBootStrap.Nav>
+          </ReactBootStrap.Navbar.Collapse>
+        </ReactBootStrap.Navbar>
 
-        ))
-        }
-      </list >
+        <list class="list">
+          <h1> Teams</h1>
+          {this.state.teams.map(team => (
+            <ol><img class="Icon" src={team.strTeamBadge} alt="Icon"></img>{team.strTeam} </ol>
+
+          ))
+          }
+        </list >
+      </div>
     );
   }
 
 }
+
