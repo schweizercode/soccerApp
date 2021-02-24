2a85e49e9d63739b0f8cf0e216ee168ffca90536
schweizercode <schweizercode@gmail.com>
2021-02-24 11:27:21 +0100

    changes

--- src/Header.js
+++ src/Header.js
@@ -2,27 +2,41 @@ import React from 'react';
 import './App.css';
 import { GiSoccerKick } from "react-icons/gi";
 
-const styleheader = {
+const styleheadline = {
     display: 'flex',
     fontSize: "130%",
     padding: '0.2%',
     paddingLeft: "8%",
     flexDirection: 'row',
     // justifyContent: 'space-around',
+}
 
+const stylelinks = {
+    display: 'flex',
+    alignSelf: 'stretch',
+    justifyContent: 'left',
+    paddingRight: '8%',
 }
 
 function Header() {
     return (
 
 
-        <div>
+        <div style={styleheadline}>
 
             <h2
-                style={styleheader}>
+            >
                 <GiSoccerKick />
             Soccer League Germany</h2>
+            <div
+                style={stylelinks}>
+                <a> Soccer League</a>
+                <a> Latest Results</a>
+                <a> Virtual League</a>
+                <a> DFL</a>
+            </div>
         </div >
+
 
     )
 }
