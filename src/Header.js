500799927098e4a023a1902fe6c2e07272693042
schweizercode <schweizercode@gmail.com>
2021-02-26 16:46:29 +0100

    fixing header

--- src/Header.js
+++ src/Header.js
@@ -2,33 +2,7 @@ import React from 'react';
 import './App.css';
 import { GiSoccerKick } from "react-icons/gi";
 import Navbar from 'react-bootstrap/Navbar'
-import { Button, Nav, Form, FormControl } from 'react-bootstrap'
-
-
-
-// function Header() {
-//     return (
-
-//         <Navbar bg="light" expand="lg">
-//             <Navbar.Brand href="#home"><GiSoccerKick /></Navbar.Brand>
-//             <Navbar.Brand href="/teams/">Soccer League Germany</Navbar.Brand>
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//             <Navbar.Collapse id="basic-navbar-nav">
-//                 <Nav className="mr-auto">
-//                     <Nav.Link href="#home">Soccer League</Nav.Link>
-//                     <Nav.Link href="#link">Latest Results</Nav.Link>
-//                     <Nav.Link href="#link">Virtual League</Nav.Link>
-//                     <Nav.Link href="#link">DFL</Nav.Link>
-
-//                 </Nav>
-//                 <Form inline>
-//                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-//                     <Button variant="outline-success">Search</Button>
-//                 </Form>
-//             </Navbar.Collapse>
-//         </Navbar>
-//     )
-// }
+import { Nav } from 'react-bootstrap'
 
 
 
@@ -36,11 +10,14 @@ function Header() {
     return (
 
         <Navbar bg="light" expand="lg">
+
             <Navbar.Brand href="#home"><GiSoccerKick /></Navbar.Brand>
             <Navbar.Brand href="/teams/">Soccer League Germany</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
+
+                <Nav className="mr-auto" style={{ textAlign: 'right' }}>
                     <Nav.Link href="leaguetable">League Table</Nav.Link>
                     <Nav.Link href="latestresults">Latest Results</Nav.Link>
                     <Nav.Link href="#link">Virtual League</Nav.Link>
@@ -49,6 +26,7 @@ function Header() {
                 </Nav>
 
             </Navbar.Collapse>
+
         </Navbar>
     )
 }
