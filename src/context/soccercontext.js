cbd1f2e97e00abe13545fcf6255607febd479cdb
schweizercode <schweizercode@gmail.com>
2021-03-02 20:11:43 +0100

    add context

--- src/context/soccercontext.js
+++ src/context/soccercontext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, setState } from 'react';
 
 export const SoccerContext = createContext()
 
@@ -8,6 +8,8 @@ export const SoccerContextProvider = ({ children }) => {
     const [data, setData] = useState()
     const [team, setTeam] = useState()
     const [loading, setLoading] = useState(false)
+    const [league, setLeague] = useState
+    const [soccer, setSoccer] = setState
 
 
     const fetchData = async () => {
@@ -30,12 +32,27 @@ export const SoccerContextProvider = ({ children }) => {
     }
 
 
+    const fetchLeague = async (idLeague) => {
+        setLoading(true)
+        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=${idLeague.toString()}&s=2020-2021`)
+        const data = await response.json()
+        console.log(data.league)
+        setLoading(false)
+        setLeague(data.League)
+    }
 
+
+    const soccer = async () => {
+        setLoading(true)
+        const response = await fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=Germany')
+        const data = await response.json()
+
+    }
 
     return (
         <SoccerContext.Provider value={{ data, setData, fetchData, team, fetchTeam, loading }}>
-            {children}
-        </SoccerContext.Provider>
+            { children}
+        </SoccerContext.Provider >
     )
 }
 
