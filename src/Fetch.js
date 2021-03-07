a5aea53b2be91268865c3c619c9ce2b18a52d4ba
schweizercode <schweizercode@gmail.com>
2021-03-07 18:19:30 +0100

    changes

--- src/Fetch.js
+++ src/Fetch.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default class Fetch extends React.Component {
     state = {
-        loading = false
+        loading = true
     };
 
     async componentDidMount() {
