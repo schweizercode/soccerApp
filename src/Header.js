715c5860bc7aa23bc6383e727ba3bc99a932e14b
schweizercode <schweizercode@gmail.com>
2021-03-09 14:59:14 +0100

    changes

--- src/Header.js
+++ src/Header.js
@@ -15,19 +15,6 @@ function Header() {
             <Navbar.Brand href="#home"><GiSoccerKick /></Navbar.Brand>
             <Navbar.Brand href="/teams/">Soccer League Germany</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-            <Navbar.Collapse id="basic-navbar-nav">
-
-                <Nav className="mr-auto" style={{ paddingLeft: '55%', }}>
-                    <Nav.Link href="leaguetable">League Table</Nav.Link>
-                    <Nav.Link href="latestresults">Latest Results</Nav.Link>
-                    <Nav.Link href="#link">Virtual League</Nav.Link>
-                    <Nav.Link href="#link">DFL</Nav.Link>
-
-                </Nav>
-
-            </Navbar.Collapse>
-
         </Navbar>
     )
 }
