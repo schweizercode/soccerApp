0ee17382a347e2c3a61bf0e136d1681f182aa6f2
schweizercode <schweizercode@gmail.com>
2021-03-10 20:05:44 +0100

    changes

---
+++ .history/src/App_20210310184111.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Header from './Header.js'
+import Nav from './Nav.js';
+import Cluboverview from './Cluboverview.js';
+import Description from './Description.js';
+import { SoccerContextProvider } from './context/soccercontext';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import ForgotPassword from './components/ForgotPassword';
+import { AuthProvider } from './context/authContext';
+import { Container } from 'react-bootstrap';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+
+
+const App = () => {
+
+  return (
+
+    <Router>
+      <AuthProvider>
+        <SoccerContextProvider>
+          <div className="App">
+            <Header />
+            <Nav />
+
+            <Switch>
+              <Route exact path="/">
+                <Cluboverview />
+              </Route>
+              <Route path="/Cluboverview">
+                <Cluboverview />
+              </Route>
+
+              <Route exact path="/Teams/:idTeam">
+                <Description />
+              </Route>
+              <Container
+                className="d-flex align-items-center justify-content-center" style={{ minHeight: "55vh", maxWidth: "500px" }}>
+                <div
+                  className="w-100">
+                  <Route path="/Register" component={Signup} />
+                  <Route path="/Login" component={Login} />
+                  <Route path="/forgot-password" component={ForgotPassword} />
+                  <Route path="/Profile" component={Profile} />
+                </div>
+              </Container>
+            </Switch>
+
+          </div >
+        </SoccerContextProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+
+export default App;
+
+
---
+++ .history/src/description_20210310184641.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useContext } from 'react';
+import './App.css';
+import { useParams, Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
+import { BsChevronDoubleLeft } from "react-icons/bs";
+import { GrInstagram, GrTwitter, GrFacebookOption } from "react-icons/gr";
+import Table from './Table.js';
+import { SoccerContext } from './context/soccercontext';
+
+
+function Description() {
+
+    const { idTeam } = useParams()
+    const { team, fetchTeam, loading } = useContext(SoccerContext)
+    console.log(team)
+
+
+    useEffect(() => {
+        fetchTeam(idTeam)
+
+    }, [])
+
+
+    const flexcontainerstyle = {
+        width: '100%',
+        paddingRight: '0',
+        paddingLeft: '0',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+    }
+
+    const flexitemcardstyle = {
+        alignItems: 'right',
+        backgroundColor: 'rgba(68, 66, 64, 0.09)',
+        height: '300px',
+        marginLeft: '10%',
+        marginRight: '10%',
+    }
+
+    const returnbuttonstyle = {
+        fontSize: '28px',
+        marginTop: '2%',
+        marginLeft: '2%',
+        marginBottom: '2%',
+        color: 'grey',
+    }
+
+
+    return (
+        <>
+            <div>
+                <div
+                    style={{ flexcontainerstyle }}>
+                    {loading === true ? (<p>Loading..</p>) : (
+
+                        <div>
+                            <Link to={`/Cluboverview/`}>
+                                <BsChevronDoubleLeft style={returnbuttonstyle} />
+                            </Link>
+
+
+                            {team &&
+                                <Card style={flexitemcardstyle}>
+                                    <Card.Header>{team.strStadium}  </Card.Header>
+                                    <Card.Body>
+                                        <div>
+                                            <Card.Title style={{ fontSize: '25px', marginLeft: '3%' }}> {team.strAlternate} </Card.Title>
+
+                                            <Card.Img style={{ width: '120px', height: '120px', display: 'flex', marginLeft: 'auto', marginRight: '5%' }} src={team.strTeamBadge}>
+                                            </Card.Img>
+
+                                        </div>
+                                    </Card.Body>
+
+                                    <div style={{ display: 'column' }}>
+                                        <Card.Body style={{ marginLeft: '1%', justifyContent: 'spaceAround', }}>
+                                            <div>
+                                                <Link to={{ pathname: `https://${team.strFacebook}` }} target="_blank" >
+                                                    <GrFacebookOption />
+                                                </Link></div>
+                                            <div>
+                                                <Link to={{ pathname: `https://${team.strInstagram}` }} target="_blank" >
+                                                    <GrInstagram />
+                                                </Link></div>
+                                            <div>
+                                                <Link to={{ pathname: `https://${team.strTwitter}` }} target="_blank" >
+                                                    <GrTwitter />
+                                                </Link></div>
+                                        </Card.Body>
+                                    </div>
+
+                                    <div>
+                                        <Button className="w-50 text-center mt-2"> <a>Description</a> </Button>
+                                        <Button className="w-50 text-center mt-2"><a>Table</a></Button>
+
+                                    </div>
+                                </Card >
+
+
+                                /* <Card.Body style={{ paddingTop: '155px' }}>
+                                    <p style={{
+                                        backgroundColor: 'rgb(220, 218, 219)'
+                                    }}> {team.strDescriptionEN}</p>
+    
+    
+                                    <h4 > Current League: {team.strLeague}</h4>
+    
+                                    <h4 style={{ backgroundColor: 'rgb(220, 218, 219)' }}> Formed Year: {team.intFormedYear}</h4>
+    
+                                    <h4 > Stadium: {team.strStadium}</h4>
+    
+                                </Card.Body> 
+
+
+
+                            }
+
+                            {/* {team &&
+                            <div style={{ display: 'flex', }}>
+
+                                <Table idLeague={team.idLeague}> </Table>
+                            </div>
+                        } */}
+                        </div>
+                    )
+                    }
+                </div >
+            </div >
+        </>
+    );
+}
+
+
+export default Description;
